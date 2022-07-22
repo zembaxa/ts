89a504a98fb2da030b7b7b3806a2d5e2d9fa4fc1
zembaxa <g.zembala@gmail.com>
2022-07-22 11:37:09 +0200

    Init

---
+++ index.ts
@@ -0,0 +1,11 @@
+// primitives
+const primitive1: string = "test";
+const primitive2: number = 123;
+const primitive3: boolean = false;
+
+// arrays
+const array1: Array<string> = ["test1", "test2"];
+const array2: string[] = ["test1", "test2"];
+
+// objects
+const object1: { x: string; y: number } = { x: "test", y: 2 };
